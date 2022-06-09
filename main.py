6c1a0041a27d29e88485a9379800bc34533bf306
nagibon338 <106874816+nagibon338@users.noreply.github.com>
2022-06-09 13:51:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget,QPushButton, QLabel, QVBoxLayout
+from random import randint
+def show_winner():
+    number = randint(0,99)
+    winner.setText(str(number))
+    text.setText('Бабиджон:')
 
 app = QApplication([])
 main_win = QWidget()
+main_win.resize(400,400)
+
+but  = QPushButton('Сгенерировать')
+text = QLabel('Кто бабиджон?')
+winner = QLabel('?')
+line = QVBoxLayout()
+
+line.addWidget(text, alignment = Qt.AlignCenter)
+line.addWidget(winner, alignment = Qt.AlignCenter)
+line.addWidget(but, alignment = Qt.AlignCenter)
+main_win.setLayout(line)
+
+
+but.clicked.connect(show_winner)
 main_win.show()
 app.exec_()
