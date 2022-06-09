929325ab5d9b89ac16c5fdb63edd368726e10de5
nagibon338 <106874816+nagibon338@users.noreply.github.com>
2022-06-09 13:26:55 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget,QPushButton, QLabel, QVBoxLayout
+
+app = QApplication([])
+main_win = QWidget()
+main_win.show()
+app.exec_()
