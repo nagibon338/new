e1e4f793bb7f2521bcd081c6307406cc7b600225
nagibon338 <106874816+nagibon338@users.noreply.github.com>
2022-06-29 12:31:24 +0300

    Update project.py

--- superjob/project.py
+++ superjob/project.py
@@ -1,8 +1,11 @@
-# напиши здесь код основного приложения и первого экрана
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from instr import *
 from second_win import *
+from final_win import *
+from PyQt5.QtGui import *
+
+
 class MainWin(QWidget):
     def __init__(self):
         super().__init__()
@@ -11,22 +14,24 @@ class MainWin(QWidget):
         self.connects()
         self.show()
     def set_appear(self):
-        self.setWindowTitle(txt_title)
+        self.setWindowTitle(title_text)
         self.resize(win_w,win_h)
+        self.move(win_x,win_y)
     def initUI(self):
-        self.hello_text = QLabel(txt_hello)
-        self.instruction = QLabel(txt_instruction)
-        self.button = QPushButton(txt_next)
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.hello_text)
-        self.layout.addWidget(self.instruction)
-        self.layout.addWidget(self.button)
-        self.setLayout(self.layout)
+        self.hello = QLabel(txt_hello)
+        self.instr = QLabel(txt_instr)
+        self.but = QPushButton(txt_but)
+        self.lay = QVBoxLayout()
+        self.lay.addWidget(self.hello,alignment = Qt.AlignCenter)
+        self.lay.addWidget(self.instr,alignment = Qt.AlignCenter)
+        self.lay.addWidget(self.but,alignment = Qt.AlignCenter)
+        self.setLayout(self.lay)
     def connects(self):
-        self.button.clicked.connect(self.next_click)
+        self.but.clicked.connect(self.next_click)
     def next_click(self):
-        self.hide()
         self.tw = TestWin()
+        self.hide()
+
 app = QApplication([])
-mw = MainWin()  
-app.exec_()    
+mw = MainWin()
+app.exec_()
